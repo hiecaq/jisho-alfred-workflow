ab64a9d5a8b6bd5fcf4ccbc50312b9d70d9c9937
quinoa42 <quinoa42@outlook.jp>
2018-10-06 17:03:08 -0700

    restructure the code to make it more reusable

--- jisho.py
+++ jisho.py
@@ -7,14 +7,16 @@ import unicodedata
 
 import requests
 
-# necessary to make sure the input is not in KFKD form
-input_string = unicodedata.normalize("NFKC", " ".join(sys.argv[1:]))
-r = requests.get(
-    f"http://beta.jisho.org/api/v1/search/words",
-    params={"keyword": input_string}
-)
-r.raise_for_status()
-output = json.loads(r.text)
+
+def get_json(input_string):
+    # necessary to make sure the input is not in KFKD form
+    normalized = unicodedata.normalize("NFKC", input_string)
+    r = requests.get(
+        f"http://beta.jisho.org/api/v1/search/words",
+        params={"keyword": normalized}
+    )
+    r.raise_for_status()
+    return json.loads(r.text)
 
 
 def get_name(item):
@@ -24,24 +26,37 @@ def get_name(item):
     )
 
 
-data = [{
-    "uid":
-        get_name(item),
-    "title":
-        get_name(item),
-    "subtitle":
-        item["japanese"][0]["reading"]
-        if "reading" in item["japanese"][0] else "",
-    "variables": {
-        "definition":
-            "\n"
-            .join([";".join(x["english_definitions"]) for x in item["senses"]])
-    },
-    "arg":
-        "https://jisho.org/search?utf8=%E2%9C%93&keyword=" + (
-            item["japanese"][0]["word"] if "word" in item["japanese"][0] else
-            item["japanese"][0]["reading"]
-        )
-} for item in output["data"][:10]]
+def map_to_items(data):
+    return {
+        "items": [{
+            "uid":
+                get_name(item),
+            "title":
+                get_name(item),
+            "subtitle":
+                item["japanese"][0]["reading"]
+                if "reading" in item["japanese"][0] else "",
+            "variables": {
+                "definition":
+                    "\n\n".join([
+                        ";".join(x["english_definitions"])
+                        for x in item["senses"]
+                    ])
+            },
+            "arg":
+                "https://jisho.org/search?utf8=%E2%9C%93&keyword=" + (
+                    item["japanese"][0]["word"]
+                    if "word" in item["japanese"][0] else
+                    item["japanese"][0]["reading"]
+                )
+        } for item in data["data"][:10]]
+    }
 
-print(json.dumps({"items": data}))
+
+def main():
+    j = get_json(" ".join(sys.argv[1:]))
+    print(json.dumps(map_to_items(j)))
+
+
+if __name__ == "__main__":
+    main()
